ee7a37fc189ac08b9fffd7188811ec3909fb6ac4
Jan Myszkier <jan@codingmice.com>
2018-03-29 10:04:07 +0200

    add support for doctrine fixtures along with example log entries, add steps in readme to inform how to load them

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ logger-symfony
 ==============
 
 A Symfony project created on March 29, 2018, 9:28 am.
+
+### Setup:
+1. run
+`cp app/config/parameters.yml.dist app/config/parameters.yml`
+2. edit `app/config/parameters.yml` and make sure the credentials match existing database user
+3. run
+`php bin/consoledoctrine:database:create`
+4. run 
+`php bin/consoledoctrine:schema:update --force`
+5. to load fixtures (optional) run:
+`php bin/console doctrine:fixtures:load -q`
+
---
+++ src/AppBundle/DataFixtures/LogFixtures.php
@@ -0,0 +1,59 @@
+<?php
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\Log;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LogFixtures extends Fixture
+{
+    public function load(ObjectManager $objectManager){
+
+        //log with GET
+        $log = new Log();
+        $log->setRequestUri('contact.html');
+        $log->setRequestMethod('GET');
+        $log->setGetParams(json_encode(['test'=>1,'param2'=>'somestring']));
+        $log->setIpAddress('127.0.0.1');
+        $log->setHttpReferer('index.html');
+        $log->setSessionId(session_id());
+        $objectManager->persist($log);
+
+        //log with POST
+        $log = new Log();
+        $log->setHttpReferer('simple-form.php');
+        $log->setRequestMethod('POST');
+        $log->setPostParams(json_encode(['postparam1'=>1,'parampost2'=>'somestring']));
+        $log->setIpAddress('0:0:0:0:0:ffff:7f00:1');
+        $log->setHttpReferer('index.html');
+        $log->setSessionId(session_id());
+        $objectManager->persist($log);
+
+        //log with PUT
+        $log = new Log();
+        $log->setRequestMethod('PUT');
+        $log->setIpAddress('999.999.999.999');
+        $log->setSessionId(session_id());
+        $objectManager->persist($log);
+
+        //log with FILES (one of them)
+        $log = new Log();
+        $log->setRequestMethod('POST');
+        $log->setPostParams(json_encode(['postparam1'=>1,'parampost2'=>'somestring']));
+        $log->setSessionId(session_id());
+        $log->setHttpReferer('upload-form.php');
+        $log->setSentFiles(json_encode([
+            'myfile' => [
+                'name' => 'test.jpg',
+                'type' => 'image/jpeg',
+                'size' => '1024',
+                'tmp_name' => '/tmp/php7zU3t5',
+                'error' => UPLOAD_ERR_OK,
+            ]
+        ]));
+        $log->setIpAddress('0:0:0:0:0:ffff:7f00:1');
+        $objectManager->persist($log);
+
+        $objectManager->flush();
+    }
+}
